a52c9c4eb5197968dbc0d50e476621c8577d494b
Rasmus <rasmus.kronberg@aalto.fi>
2020-05-10 14:27:09 +0300

    Added hyperparameter grid search

---
+++ rndForest.py
@@ -0,0 +1,235 @@
+
+"""
+Random Forest ML implementation
+Includes options for cross-validation,
+hyperparameter gridsearch, calculation 
+of SHAP feature importances and plotting
+
+author: Rasmus Kronberg
+"""
+
+# Load necessary packages
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import StratifiedKFold, GridSearchCV, train_test_split
+from sklearn.metrics import mean_absolute_error as MAE
+from sklearn.metrics import mean_squared_error as MSE
+from sklearn.metrics import r2_score as R2
+import pandas as pd
+import numpy as np
+import argparse
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import shap
+
+def doSHAP(rf,x_train):
+
+    i = 0
+    shap_corrs = []
+    shap_values = shap.TreeExplainer(rf).shap_values(x_train)
+    shap_imps = np.mean(abs(shap_values),axis=0)
+    for feature in x_train[0]:
+        shap_corrs.append(np.corrcoef(shap_values[:,i],x_train[:,i])[1,0])
+        i += 1
+
+    return shap_imps, shap_corrs
+
+def doPlotSHAP(shap_i,shap_c,featureNames):
+
+    shap_ave = np.mean(shap_i,axis=0)
+    corr_ave = np.mean(shap_c,axis=0)
+    shap_std = np.std(shap_i,axis=0,ddof=1)
+
+    ind_shap = np.argsort(shap_ave)
+
+    featureSym=[r'RMSD',r'$x_\mathrm{N}$',r'$Z$',r'CN$_\mathrm{ad}$',r'$\Delta$CN$_\mathrm{ad}$',
+                    r'$\min\{\theta\}$',r'$\max\{\theta\}$',r'$\min\{d\}$',r'$\langle d\rangle$',
+                    r'CN$_\mathrm{N}$',r'$\Delta$CN$_\mathrm{N}$',r'$x_\mathrm{V}$',
+                    r'$\Delta\theta$',r'$M$',r'$n$',r'$m$']
+
+    print('Feature importances (5-CV Shapley, positive or negative correlation):')
+    fig,ax=plt.subplots(figsize=(6,6))
+    for i in ind_shap:
+        if corr_ave[i] >= 0:
+            print('%s: %s (+)' % (featureNames[i],shap_ave[i]))
+            plt.barh(featureNames[i],shap_ave[i],xerr=shap_std[i],capsize=3,error_kw={'elinewidth':1},color='C3')
+        else:
+            print('%s: %s (-)' % (featureNames[i],shap_ave[i]))
+            plt.barh(featureNames[i],shap_ave[i],xerr=shap_std[i],capsize=3,error_kw={'elinewidth':1},color='C9',label=r'$R_{ij}<0$')
+
+    plt.minorticks_on()
+    plt.gca().tick_params(which='both',direction='in',top=True,left=False)
+    plt.xlabel(r'$\langle|\phi_j|\rangle$')
+    plt.subplots_adjust(left=0.20,bottom=0.13)
+    for tick in ax.yaxis.get_major_ticks():
+        tick.label.set_fontsize(18) 
+    plt.xticks([0,0.03,0.06,0.09])
+    plt.xlim(left=0)
+
+    red_patch = mpatches.Patch(color='C3',label=r'Pos. Corr.')
+    blue_patch = mpatches.Patch(color='C9',label=r'Neg. Corr.')
+    plt.legend(handles=[red_patch,blue_patch],frameon=False,loc='lower right')
+
+def crossval(x,y,model,strat):
+
+    n_splits=5
+    kf = StratifiedKFold(n_splits=n_splits,shuffle=True,random_state=rnd)
+
+    error_test = []
+    error_train = []
+
+    error_R2_test = []
+    error_R2_train = []
+
+    shap_i = []
+    shap_c = []
+
+    for train, test in kf.split(x,strat):
+        model.fit(x[train], y[train])
+        y_pred_test = model.predict(x[test])
+        y_pred_train = model.predict(x[train])
+        error_test.append(np.sqrt(MSE(y[test],y_pred_test)))
+        error_train.append(np.sqrt(MSE(y[train],y_pred_train)))
+        error_R2_test.append(model.score(x[test], y[test]))
+        error_R2_train.append(model.score(x[train], y[train]))
+
+        if(True):
+            shap_imps, shap_corrs = doSHAP(model,x[train])
+            shap_i.append(shap_imps)
+            shap_c.append(shap_corrs)
+
+    score_test = np.mean(error_test)
+    score_test_std = np.std(error_test, ddof=1)
+    score_train = np.mean(error_train)
+    score_train_std = np.std(error_train, ddof=1)
+
+    score_R2_test = np.mean(error_R2_test)
+    score_R2_test_std = np.std(error_R2_test, ddof=1)
+    score_R2_train = np.mean(error_R2_train)
+    score_R2_train_std = np.std(error_R2_train, ddof=1)
+
+    print('R2 score (Training set, CV): %s +- %s' % (score_R2_train,score_R2_train_std))
+    print('R2 score (Test set, CV): %s +- %s' % (score_R2_test,score_R2_test_std))
+    print('RMSE (Training set, CV): %s +- %s eV' % (score_train,score_train_std))
+    print('RMSE (Test set, CV): %s +- %s eV' % (score_test,score_test_std))
+
+    return(y[train],y_pred_train,y[test],y_pred_test,shap_i,shap_c)
+
+def gridsearch(x,y,strat):
+    x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2,
+        stratify=strat,shuffle=True,random_state=rnd)
+
+    rf=GridSearchCV(RandomForestRegressor(oob_score=True,random_state=rnd),cv=5,
+        param_grid={"n_estimators": np.linspace(500,500,1,dtype=int),
+        "max_features": np.linspace(1,len(x[0,:]),len(x[0,:]+1),dtype=int)})
+
+    rf.fit(x_train,y_train)
+    print('Best parameters from gridsearch: %s' % rf.best_params_)
+
+def doPlot(y,y_train,y_pred_train,y_test,y_pred_test):
+
+    fig,ax=plt.subplots(figsize=(7,6))
+    plt.plot([-3,3],[-3,3],'k--')
+    plt.plot(y_train,y_pred_train,'C9o',fillstyle='none',markeredgewidth=1.5,
+        label=r'Training set')
+    plt.plot(y_test,y_pred_test,'C3x',markeredgewidth=1.5,label=r'Test set')
+    plt.xlim(np.amin(y)-0.1,np.amax(y)+0.1)
+    plt.ylim(np.amin(y)-0.1,np.amax(y)+0.1)
+    plt.minorticks_on()
+    plt.gca().tick_params(which='both',direction='in',right=True,top=True)
+    plt.ylabel(r'$\Delta E_\mathrm{RF}$ (eV)')
+    plt.xlabel(r'$\Delta E_\mathrm{DFT}$ (eV)')
+    plt.subplots_adjust(left=0.17,bottom=0.13)
+    plt.legend(frameon=False,handletextpad=0.1,loc='upper left')
+
+    plt.show()
+
+
+def line():
+    print('\n========================\n')
+
+def main():
+
+    # Get the data
+    line()
+    data = pd.read_csv(args['input'])
+
+    print("Data types in dataframe:")
+    print(data.dtypes)
+
+    # Select the features
+    featureNames=np.array(['rmsd','cN','cV','Zsite','dmin','dave','mult','n','m','refnearNcoord',
+        'diffnearNcoord','refAcoord','diffAcoord','minAng','maxAng','angDisp'])
+    nFeatures=len(featureNames)
+    nSamples=len(data) 
+    x=np.zeros((nSamples,nFeatures))
+    i=0
+    for featureName in featureNames:
+        x[:,i]=data[featureName].values
+        i+=1
+
+    # Target variable
+    y=data['E_ad'].values
+
+    # Stratify based on adsorption energies for balanced train-test splits
+    strat=np.around(y)
+
+    print('Finished reading data, length of data: %s' % len(data))
+
+    # Describe the data
+    line()
+    print('Metadata:')
+    print(data.describe())
+
+    # Search best parameters for n_estimators, max_features?
+    if(False):
+        gridsearch(x,y,strat)
+        quit()
+
+    # Predicting numerical values - Regression model with Random Forest
+    # Train and test
+    line()
+    print('RANDOM FOREST REGRESSOR')
+    print('Predicting numerical values for training and test set:')
+
+    rf = RandomForestRegressor(n_estimators=20, max_features=None,
+        oob_score=True,random_state=rnd)
+
+    # Do cross-validation?
+    if(True):
+        y_train,y_pred_train,y_test,y_pred_test,shap_i,shap_c = crossval(x,y,rf,strat)
+    else:
+        x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2,
+            stratify=strat,shuffle=True,random_state=rnd)
+        rf.fit(x_train,y_train)
+        print('Score (training set) R2: %s' % rf.score(x_train, y_train))
+        print('Score (test set) R2: %s' % rf.score(x_test, y_test))
+        y_pred_test=rf.predict(x_test)
+        y_pred_train=rf.predict(x_train)
+        print('RMSE (training set): %s eV' % np.sqrt(MSE(y_train,y_pred_train)))
+        print('RMSE (test set): %s eV' % np.sqrt(MSE(y_test,y_pred_test)))
+
+    # Calculate and plot Shapley importances?
+    if(True):
+        line()
+        doPlotSHAP(shap_i,shap_c,featureNames) 
+
+    line()
+
+    # Plot predictions vs. training and test sets?
+    if(True):
+        doPlot(y,y_train,y_pred_train,y_test,y_pred_test)
+
+if __name__ == '__main__':
+    plt.rc('text', usetex=True)
+    plt.rc('font', family='serif', size=24)
+    plt.rc('axes', linewidth=1.5)
+    plt.rc('lines', linewidth=1.5)
+    plt.rc('xtick.major',width=1.5,size=7)
+    plt.rc('xtick.minor',width=1.5,size=4)
+    plt.rc('ytick.major',width=1.5,size=7)
+    plt.rc('ytick.minor',width=1.5,size=4)
+    rnd=1
+    parser = argparse.ArgumentParser(description='Random Forest Machine Learning')
+    parser.add_argument('-i','--input',help='Input data')
+    args = vars(parser.parse_args())
+    main()
